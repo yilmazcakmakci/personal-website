98e275ce876c18f23ee84e34a7eb9b95f91dfe76
Yılmaz Çakmakçı <cakmakcy@gmail.com>
2023-03-05 23:52:17 +0300

    fix focus endpoint

--- pages/api/focus.js
+++ pages/api/focus.js
@@ -7,14 +7,19 @@ export default async function handler(req, res) {
 
         if (req.method === 'POST') {
             console.log(req.body)
-            const { type, project } = req.body
 
+            let projectName
             const table = new Table('Working')
-            const latestRecord = await table.getLatest()
-            const projectName = project || latestRecord.fields.project
+
+            if (req.body.project) {
+                projectName = req.body.project
+            } else {
+                const latestRecord = await table.getLatest()
+                projectName = latestRecord.fields.project
+            }
 
             const payload = {
-                record_type: type === 'Work' ? 'start' : 'end',
+                record_type: req.body.type === 'Work' ? 'start' : 'end',
                 date: new Date(),
                 working_type: ['dmc', 'enduser'].includes(projectName)
                     ? 'work'
@@ -27,5 +32,6 @@ export default async function handler(req, res) {
         }
     } catch (error) {
         res.status(400).json({ message: 'Error' })
+        console.error(error)
     }
 }
